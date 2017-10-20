108c97ca38a90b3002c76667a2ea4e6425609c8e
DenBy1726 <realdenby@gmail.com>
2017-10-20 12:00:22 +0300

    test 11

--- task/11-katas-2-tasks.js
+++ task/11-katas-2-tasks.js
@@ -34,7 +34,64 @@
  *
  */
 function parseBankAccount(bankAccount) {
-    throw new Error('Not implemented');
+    let primitive = ['   ',' _ ','  |',' _|','|_|','|_ ','| |'];
+   // let header = [1,0,1,1,0,1,1,1,1,1];
+   // let center = [6,2,3,3,4,5,5,2,4,4];
+  //  let footer = [4,2,5,3,2,3,4,2,4,3];
+    let pattern = [[1,6,4],[0,2,2],[1,3,5],[1,3,3],[0,4,2],[1,5,3],[1,5,4],[1,2,2],[1,4,4],[1,4,3]];
+
+   let accountStrings = bankAccount.split('\n').map((x)=>{
+       let triplet = [];
+       let buff = "";
+       x.split("").forEach((x,i,arr)=>{
+           if(i%3===0 && i !== 0){
+               triplet.push(buff);
+               buff = "";
+           }
+           buff += x;
+       });
+       if(buff.length === 3)
+           triplet.push(buff);
+       return triplet;
+    }).filter((x)=>x.length > 0);
+   let rez = "";
+   for(let i=0;i<accountStrings[0].length;i++) {
+       let header = primitive.indexOf(accountStrings[0][i]);
+       let center = primitive.indexOf(accountStrings[1][i]);
+       let footer = primitive.indexOf(accountStrings[2][i]);
+
+       rez += find(pattern,[header,center,footer]);
+   }
+   let patternItems = rez.split(0).filter((x)=>x.length > 0);
+
+   if(patternItems.length === 0)
+       return 0;
+    return Number.parseInt(patternItems.join("0"));
+
+}
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a === null || b === null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+function find(arr1,arr2) {
+    let i = 0;
+    for (let sub of arr1) {
+        if (arraysEqual(sub, arr2)) { // found it
+            return i;
+        }
+        i++;
+    }
 }
 
 
@@ -63,7 +120,22 @@ function parseBankAccount(bankAccount) {
  *                                                                                                'characters.'
  */
 function* wrapText(text, columns) {
-    throw new Error('Not implemented');
+    let arr = text.split(' ');
+    let rez = [];
+    let contentLength = 0;
+    for(let i=0;i<arr.length;){
+        if(contentLength + arr[i].length + rez.length <= columns){
+            rez.push(arr[i]);
+            contentLength += arr[i].length;
+            i++;
+        }
+        else{
+            yield rez.join(' ');
+            contentLength = 0;
+            rez= [];
+        }
+    }
+    yield rez.join(' ');
 }
 
 
@@ -97,11 +169,91 @@ const PokerRank = {
     TwoPairs: 2,
     OnePair: 1,
     HighCard: 0
-}
+};
+
+let nums = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let signes = ['♣','♦','♥','♠'];
 
 function getPokerHandRank(hand) {
-    throw new Error('Not implemented');
+    let cards = [];
+    for (let card of hand) {
+        cards.push(parseRank(card));
+    }
+    let signSame = isSignSame(cards);
+    let straight = isStraight(cards);
+    let freq = countFrequent(cards);
+
+    if (signSame && straight)
+        return PokerRank.StraightFlush;
+    if (freq[3] === 1)
+        return PokerRank.FourOfKind;
+    if (freq[2] === 1 && freq[1] === 1)
+        return PokerRank.FullHouse;
+    if (signSame)
+        return PokerRank.Flush;
+    if (straight)
+        return PokerRank.Straight;
+    if (freq[2] === 1)
+        return PokerRank.ThreeOfKind;
+    if(freq[1] === 2)
+        return PokerRank.TwoPairs;
+    if(freq[1] === 1)
+        return PokerRank.OnePair;
+    else
+        return PokerRank.HighCard;
 }
+
+function parseRank(value){
+
+    let num = value.slice(0,-1);
+    let sign = value.slice(-1);
+    let num_i = nums.indexOf(num);
+    let sign_i = signes.indexOf(sign);
+    return [num_i,sign_i];
+}
+
+function isSignSame(cards){
+    for(let i=0;i<cards.length-1;i++){
+        if(cards[i][1] !== cards[i+1][1])
+            return false;
+    }
+    return true;
+}
+
+function isStraight(cards){
+    let values = cards.map((x)=>x[0]).filter(x=>x !== 0);
+    values.sort((x,y)=>x > y);
+    for(let i=0;i<values.length-1;i++){
+        if(values[i+1] != values[i]+1)
+            return false;
+    }
+    if(values.length === 4){
+        if(values[0] === 1 || values[values.length-1] === 12)
+            return true;
+        else
+            return false;
+    }
+    return true;
+}
+
+function countFrequent(cards){
+    let values = cards.map((x)=>x[0]);
+    values.sort();
+    let map = new Map();
+    for(let i=0;i<values.length;i++){
+        if(map.has(values[i]) === false)
+            map.set(values[i],1);
+        else
+            map.set(values[i],map.get(values[i])+1);
+    }
+    let rez = new Array(5).fill(0);
+    for (let it of map) {
+        rez[it[1]-1]++;
+    }
+    return rez;
+}
+
+
 
 
 /**
